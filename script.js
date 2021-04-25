fa7886cf3959e4b0492848079dc4bf7a65a0936f
ragafatwa <ragafatwa@gmail.com>
2021-04-25 14:56:37 +0700

    working rgb

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+
+// navbar
 const menuToggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('nav ul');
 
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
 });
+
+// scrollbar
+let progress = document.getElementById('progressbar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function(){
+    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
+}
