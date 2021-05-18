be88771124f25b9338fe40b5f9d35fcf0adf59d6
ragafatwa <ragafatwa@gmail.com>
2021-05-18 13:50:35 +0700

    fixed profile pict

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 
 // navbar
 const menuToggle = document.querySelector('.menu-toggle input');
+const menuSpan1 = document.querySelector('.menu-toggle span:nth-child(2)');
+const menuSpan2 = document.querySelector('.menu-toggle span:nth-child(4)');
+const menuSpan3 = document.querySelector('.menu-toggle span:nth-child(3)');
 const nav = document.querySelector('nav ul');
+const navLink = document.querySelector('nav-link');
 
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
 });
+navLink.addEventListener('click', function() {
+    menuSpan1.classList.toggle('toggle-anim1');
+    menuSpan2.classList.toggle('toggle-anim2');
+    menuSpan3.classList.toggle('toggle-anim3');
+});
+
 
 // scrollbar
 let progress = document.getElementById('progressbar');
