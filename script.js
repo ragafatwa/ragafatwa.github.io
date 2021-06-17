b8ae60207606fd5ac374c5f0211e15bf82db7b86
ragafatwa <ragafatwa@gmail.com>
2021-06-17 18:16:07 +0700

    little changes

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
 // navbar
 const menuToggle = document.querySelector('.menu-toggle input');
+const menuSpan1 = document.querySelector('.menu-toggle span:nth-child(2)');
+const menuSpan2 = document.querySelector('.menu-toggle span:nth-child(4)');
+const menuSpan3 = document.querySelector('.menu-toggle span:nth-child(3)');
 const nav = document.querySelector('nav ul');
 
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
+    menuSpan1.classList.toggle('toggle-anim1');
+    menuSpan2.classList.toggle('toggle-anim2');
+    menuSpan3.classList.toggle('toggle-anim3');
 });
 
 // scrollbar
