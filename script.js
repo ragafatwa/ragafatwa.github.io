7a0741e219690cf45098a2259097b060f7f63c10
ragafatwa <ragafatwa@gmail.com>
2021-07-01 22:13:01 +0700

    navbar fixed

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-
-// navbar
+// Nav
 const menuToggle = document.querySelector('.menu-toggle input');
 const menuSpan1 = document.querySelector('.menu-toggle span:nth-child(2)');
 const menuSpan2 = document.querySelector('.menu-toggle span:nth-child(4)');
 const menuSpan3 = document.querySelector('.menu-toggle span:nth-child(3)');
 const nav = document.querySelector('nav ul');
-const navLink = document.querySelector('nav-link');
 
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
@@ -13,17 +11,4 @@ menuToggle.addEventListener('click', function() {
     menuSpan2.classList.toggle('toggle-anim2');
     menuSpan3.classList.toggle('toggle-anim3');
 });
-navLink.addEventListener('click', function() {
-    menuSpan1.classList.toggle('toggle-anim1');
-    menuSpan2.classList.toggle('toggle-anim2');
-    menuSpan3.classList.toggle('toggle-anim3');
-});
 
-
-// scrollbar
-let progress = document.getElementById('progressbar');
-let totalHeight = document.body.scrollHeight - window.innerHeight;
-window.onscroll = function(){
-    let progressHeight = (window.pageYOffset / totalHeight) * 100;
-    progress.style.height = progressHeight + "%";
-}
