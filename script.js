e2166e9e2a12dcf47e0c3f2046eae9ada8761e97
ragafatwa <ragafatwa@gmail.com>
2022-08-06 00:07:49 +0700

    web update

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-
-// navbar
+// Nav
 const menuToggle = document.querySelector('.menu-toggle input');
 const menuSpan1 = document.querySelector('.menu-toggle span:nth-child(2)');
 const menuSpan2 = document.querySelector('.menu-toggle span:nth-child(4)');
 const menuSpan3 = document.querySelector('.menu-toggle span:nth-child(3)');
 const nav = document.querySelector('nav ul');
-const navLink = document.querySelector('nav-link');
+
+let cursorMeter = document.getElementById('cursorMeter');
+document.addEventListener('mousemove', function(e){
+    cursorMeter.style.top = e.clientY+'px';
+    cursorMeter.style.left = e.clientX+'px';
+});
+
+let percent = document.getElementById('percent');
+let progressBar = document.getElementById('progressBar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+
+window.onscroll = function() {
+    let progress = (window.pageYOffset / totalHeight) * 100;
+    progressBar.style.width = progress + '%';
+};
 
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
-});
-navLink.addEventListener('click', function() {
     menuSpan1.classList.toggle('toggle-anim1');
     menuSpan2.classList.toggle('toggle-anim2');
     menuSpan3.classList.toggle('toggle-anim3');
 });
 
+// img.addEventListener('mouseover', function() {
+//     rgb.classList.toggle('d-flex')
+// });
 
-// scrollbar
-let progress = document.getElementById('progressbar');
-let totalHeight = document.body.scrollHeight - window.innerHeight;
-window.onscroll = function(){
-    let progressHeight = (window.pageYOffset / totalHeight) * 100;
-    progress.style.height = progressHeight + "%";
-}
+var player = videojs('videoPlayer', {
+    controls: true,
+    fluid: true,
+    playbackRates: [0.25, 0.5, 1, 1.25, 1.5, 2],
+});
